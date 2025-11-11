156bda2e6d5236df673c4405288cc34d40d263b2
Yashika-2468 <gyashika2005@gmail.com>
2025-11-11 19:29:10 +0530

    Add files via upload

---
+++ Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+function Navbar() {
+  return (
+    <div>
+        <nav>
+  <div class="logo">MySite</div>
+  <a href="#">Home</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Contact</a>
+</nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
